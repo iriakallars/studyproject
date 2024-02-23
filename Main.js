49499bacc04cd71b641ae808fb8ae46e250b2a0b
Iris Pillard <161033374+iriakallars@users.noreply.github.com>
2024-02-23 14:23:43 +0200

    Create Main.js

---
+++ Main.js
@@ -0,0 +1,23 @@
+  let settings = global.db.data.settings[this.user.jid]
+            if (typeof settings !== "object") global.db.data.settings[this.user.jid] = {}
+            if (settings) {
+                if (!("self" in settings)) settings.self = false
+                if (!("autoread" in settings)) settings.autoread = false
+                if (!("restrict" in settings)) settings.restrict = false
+                if (!("restartDB" in settings)) settings.restartDB = 0
+                if (!("status" in settings)) settings.status = 0
+
+
+  if (opts["queque"] && m.text && !(isMods || isPrems)) {
+            let queque = this.msgqueque,
+                time = 1000 * 5
+            const previousID = queque[queque.length - 1]
+            queque.push(m.id || m.key.id)
+            setInterval(async function() {
+                if (queque.indexOf(previousID) === -1) clearInterval(this)
+                await delay(time)
+            }, time)
+        }
+         if (process.env.MODE && process.env.MODE.toLowerCase() === 'private' && !(isROwner || isOwner))
+          return;
+
